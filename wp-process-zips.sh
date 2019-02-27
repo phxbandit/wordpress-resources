6b02df9debec5d33a79357e258dce66eb0c8a501
WSTNPHX <WSTNPHX@users.noreply.github.com>
2019-02-27 11:59:39 -0700

    updated url w/ variable

--- wp-process-zips.sh
+++ wp-process-zips.sh
@@ -20,7 +20,7 @@ download() {
         txt="$HOME/$urls"
     else
         txt="$urls"
-        wget -q https://raw.githubusercontent.com/phxbandit/wordpress-resources/master/wordpress-release-urls.txt
+        wget -q "https://raw.githubusercontent.com/phxbandit/wordpress-resources/master/$urls"
     fi
 
     for i in $(cat "$txt"); do
