5fdace55195777ff0378386023f863b437bf026d
WSTNPHX <WSTNPHX@users.noreply.github.com>
2019-02-28 11:57:57 -0700

    added md5 generation

--- wp-gen-md5s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# wp-gen-md5s.sh
-# ./wp-gen-md5s.sh | tee wordpress-md5s
-for i in $(ls); do
-    if [[ ! "$i" =~ "wp-process-zips.sh" ]] && [[ ! "$i" =~ "wp-gen-md5s.sh" ]] && [[ ! "$i" =~ "wordpress-md5s" ]]; then
-        find "$i" -type f -not -path "*wp-content*" | xargs md5sum
-    fi
-done
--- wp-process-zips.sh
+++ wp-process-zips.sh
@@ -3,12 +3,12 @@
 # phxbandit
 
 help() {
-    echo "wp-process-zips.sh - Downloads and processes WordPress zips for wp-md5.sh"
+    echo "wp-process-zips.sh - Downloads WordPress and generates MD5s for wp-md5.sh"
     echo "Usage: ./wp-process-zips.sh [-a|-d|-h|-p]"
     echo "  -a = All, download and process"
     echo "  -d = Download zips only"
     echo "  -h = This help"
-    echo "  -p = Process zips only"
+    echo "  -p = Process, extract zips and generate MD5s"
 }
 
 download() {
@@ -42,11 +42,19 @@ download() {
 }
 
 process() {
+    wpmd5s='wordpress-md5s'
+
     for j in *.zip; do
         dir=$(unzip -l "$j" | head -4 | tail -1 | awk '{print $4}' | sed -e 's/\/$//')
         ver=$(echo -n "$j" | sed -e 's/wordpress-//' | sed -e 's/\.zip$//')
         echo "Processing $j..."
         unzip -q "$j" && mv "$dir" "$ver" && rm "$j"
+    done
+
+    for k in $(ls); do
+        if [[ ! "$k" =~ 'wp-process-zips.sh' ]] && [[ ! "$k" =~ 'wordpress-release-urls.txt' ]] && [[ ! "$k" =~ "$wpmd5s" ]]; then
+            find "$k" -type f -not -path "*wp-content*" | xargs md5sum >> "$wpmd5s"
+        fi
     done
 }
 
