9492d43fa5d700975588558af90c605086a9cb69
phxbandit <875444+phxbandit@users.noreply.github.com>
2023-07-06 14:36:28 -0700

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -2,17 +2,20 @@
 
 # wp.sh - Replaces wp core files
 
+# Todo: make random name creation a function
+
 # Define
 adm='wp-admin'
 inc='wp-includes'
+ind='index.php'
 verphp="$inc/version.php"
 
 # Check for existing wp
 if [ -f "$verphp" ]; then
     ver="$(grep 'wp_version =' $verphp | awk -F"'" '{print $2}')"
     echo -e "\nFound wp version $ver\n"
-    read -p "Continue replacing wp $ver? (y/n) " ans
-    if [ "$ans" != 'y' ]; then
+    read -p "Continue replacing wp $ver? (y/n) " ans1
+    if [ "$ans1" != 'y' ]; then
         echo -e "\nExiting\n"
         exit
     fi
@@ -48,8 +51,25 @@ mv "$rel/$adm" "$adm"
 echo "Restoring wp-includes..."
 mv "$rel/$inc" "$inc"
 
-echo "Restoring php files..."
-cp -f "$rel"/*.php .
+# Verify user wants to write index.php and other wp php files
+if [ -f "$ind" ]; then
+    lslh=$(ls -lh "$ind")
+    echo -e "\n$ind detected:"
+    echo -e "$lslh\n"
+    read -p "Continue replacing $ind and php files? (y/n) " ans2
+    if [ "$ans2" == 'y' ]; then
+        echo -e "\nRestoring php files..."
+        dis4="DISABLED${RANDOM}"
+        mv "$ind" "$ind-$dis4"
+        cp -f "$rel"/*.php .
+    else
+        echo -e "\nExiting\n"
+        exit
+    fi
+else
+    echo "Restoring php files..."
+    cp -f "$rel"/*.php .
+fi
 
 echo "Cleaning up..."
 rm -rf "$rel"
