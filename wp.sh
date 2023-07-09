468157672f2ced48c641dc93e16ecdfbad1eb392
phxbandit <875444+phxbandit@users.noreply.github.com>
2023-07-09 11:07:53 -0700

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -2,17 +2,20 @@
 
 # wp.sh - Replaces wp core files
 
-# Todo: make random name creation a function
+function rand_name() {
+    local dis_name="disabled-${RANDOM}"
+    echo "$dis_name"
+}
 
 # Define
 adm='wp-admin'
 inc='wp-includes'
 ind='index.php'
-verphp="$inc/version.php"
+ver_php="$inc/version.php"
 
 # Check for existing wp
-if [ -f "$verphp" ]; then
-    ver="$(grep 'wp_version =' $verphp | awk -F"'" '{print $2}')"
+if [ -f "$ver_php" ]; then
+    ver="$(grep 'wp_version =' $ver_php | awk -F"'" '{print $2}')"
     echo -e "\nFound wp version $ver\n"
     read -p "Continue replacing wp $ver? (y/n) " ans1
     if [ "$ans1" != 'y' ]; then
@@ -20,7 +23,7 @@ if [ -f "$verphp" ]; then
         exit
     fi
 else
-    echo -e "\n$verphp not found\n"
+    echo -e "\n$ver_php not found\n"
     read -p "Enter wp version to install: " ver
 fi
 
@@ -28,24 +31,21 @@ rel="wordpress-$ver"
 url="https://wordpress.org/$rel.zip"
 
 if [ -d "$adm" ]; then
-    dis1="DISABLED${RANDOM}"
-    mv "$adm" "$adm-$dis1"
+    mv "$adm" "$adm-$(rand_name)"
 fi
 
 if [ -d "$inc" ]; then
-    dis2="DISABLED${RANDOM}"
-    mv "$inc" "$inc-$dis2"
+    mv "$inc" "$inc-$(rand_name)"
 fi
 
 if [ -d 'wordpress' ]; then
-    dis3="DISABLED${RANDOM}"
-    mv wordpress "wordpress-$dis3"
+    mv wordpress "wordpress-$(rand_name)"
 fi
 
 wget "$url"
 unzip "$rel.zip" && mv wordpress "$rel" && rm "$rel.zip"
 
-echo "Restoring wp-admin..."
+echo -e "\nRestoring wp-admin..."
 mv "$rel/$adm" "$adm"
 
 echo "Restoring wp-includes..."
@@ -59,8 +59,7 @@ if [ -f "$ind" ]; then
     read -p "Continue replacing $ind and php files? (y/n) " ans2
     if [ "$ans2" == 'y' ]; then
         echo -e "\nRestoring php files..."
-        dis4="DISABLED${RANDOM}"
-        mv "$ind" "$ind-$dis4"
+        mv "$ind" "$ind-$(rand_name)"
         cp -f "$rel"/*.php .
     else
         echo -e "\nExiting\n"
